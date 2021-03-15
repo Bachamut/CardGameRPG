f0d18d72e87edaabfb199b530d05315a7c29d553
Bachamut <lewand.luk@gmail.com>
2021-03-15 23:14:29 +0100

    initial commit

---
+++ assets/lib/game_logic.py
@@ -0,0 +1,48 @@
+from game_object.game_object import GameObject
+
+from assets.lib.card_manager import CardManager
+from assets.lib.character_manager import CharacterManager
+from assets.lib.item_manager import ItemManager
+
+
+class GameLogic(GameObject):
+
+    current_player = None
+    players_list = []
+    initialized = False
+
+    def __init__(self):
+        super(GameLogic, self).__init__()
+        self.card_container = None
+
+    def _initialize(self):
+        GameLogic.initialized = True
+        CardManager.load_config('assets/lib/card_types.json')
+        CharacterManager.load_config('assets/lib/character_types.json')
+        ItemManager.load_config('assets/lib/item_types.json')
+        self.card_container = GameObject.get_object_pool().select_with_label('CardContainer')[0]
+        self.card_container.on_create()
+
+        player = CharacterManager.create_character("character_edward")
+        goblin = CharacterManager.create_character("character_goblin")
+        GameLogic.players_list.append(player)
+        GameLogic.players_list.append(goblin)
+        GameLogic.current_player = GameLogic.players_list[0]
+
+        GameLogic.current_player.inventory.add_item('Short sword')
+        GameLogic.current_player.add_equip('hand_r', 'Short sword')
+
+    def on_create(self):
+        pass
+
+    def on_script(self):
+        if not GameLogic.initialized and len(GameObject.get_object_pool().select_with_label('CardContainer')) != 0:
+            self._initialize()
+        else:
+            pass
+
+    def on_event(self, event):
+        pass
+
+
+
---
+++ card_game_rpg.py
@@ -0,0 +1,3 @@
+from grid2d.main import main
+
+main()
