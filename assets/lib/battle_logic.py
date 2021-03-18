718ba3641339a18f90ed741f56f9cbb78e953b54
Bachamut <lewand.luk@gmail.com>
2021-03-18 02:23:00 +0100

    model view controller implementation

--- assets/lib/battle_logic.py
+++ assets/lib/battle_logic.py
@@ -1,27 +1,49 @@
-from assets.lib.card_manager import CardManager
-import random
+from assets.lib.game_logic import GameLogic
+from game_object.game_object import GameObject
 
-class BattleLogic():
 
-    # def __init__(self):
-        # self.battledeck = []
-        # self.hand = []
+class BattleLogic(GameObject):
 
-    @staticmethod
-    def create_battledeck(character):
-        for card_instance, quantity in character.deck.items():
-            for card in range(0, quantity):
-                card = CardManager.create_card(card_instance)
-                character.battledeck.append(card)
 
-    @staticmethod
-    def card_draw(character, draw_pile):
-        card = draw_pile.pop(0)
-        character.hand.append(card)
+    _initialized = False
+    started = False
+    current_character = None
+    current_target = None
+    selected_card = None
 
-    @staticmethod
-    def get_hand(character, draw_amount=3):
-        draw_pile = random.sample(character.battledeck, len(character.battledeck))
-        for draw_card in range(0, draw_amount):
-            BattleLogic.card_draw(character, draw_pile)
+    character_model_active = False
+    card_model_active = False
 
+    def __init__(self):
+        super(BattleLogic, self).__init__()
+
+    def _initialize(self):
+        BattleLogic._initialized = True
+
+    def on_script(self):
+        if not self._initialized and GameLogic._initialized:
+            self._initialize()
+        else:
+            pass
+
+        if BattleLogic.started == False and len(GameObject.get_object_pool().select_with_label('CharacterModel')) != 0:
+            BattleLogic.started = True
+            # Generating party and initial character order
+            character_model = GameObject.get_object_pool().select_with_label('CharacterModel')[0]
+            character_model.create_party()
+            character_model.create_enemies()
+            character_model.update_order()
+            BattleLogic.current_character = character_model.get_next()
+
+            card_model = GameObject.get_object_pool().select_with_label('CardModel')[0]
+            for character in character_model.order_list:
+                card_model.create_battledeck(character)
+
+            BattleLogic.character_model_active = False
+            BattleLogic.card_model_active = True
+
+        elif BattleLogic.started == True:
+            pass
+
+    def on_event(self, event):
+        pass
--- assets/lib/item_container.py
+++
@@ -1,7 +0,0 @@
-from game_object.game_object import GameObject
-
-
-class ItemContainer(GameObject):
-
-    def __init__(self):
-        super(ItemContainer, self).__init__()
