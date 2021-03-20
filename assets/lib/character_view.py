c089d83a38a59abf820411ed314f5823ed302a98
Ocelpers <mat.lewandowski@gmail.com>
2021-03-21 00:35:24 +0100

    Players and Enemies display

--- assets/lib/character_view.py
+++ assets/lib/character_view.py
@@ -1,3 +1,4 @@
+from assets.lib.battle_logic import BattleLogic
 from assets.lib.character_manager import CharacterManager
 from assets.lib.game_logic import GameLogic
 from game_object.game_object import GameObject
@@ -12,7 +13,8 @@ class CharacterView(GameObject):
         self._initialized = False
         self.position = None
 
-
+        self.players = None
+        self.enemies = None
 
     def on_create(self):
         self.position = self.property('TransformProperty').position
@@ -21,6 +23,19 @@ class CharacterView(GameObject):
 
     def _initialize(self):
         self._initialized = True
+
+        self.players = GameLogic.get_sprite_pool().select_with_label('Players')[0]
+        self.enemies = GameLogic.get_sprite_pool().select_with_label('Enemies')[0]
+
+        self.enemies.property('TransformProperty').position.x = 512
+
+        self.attach_child(self.players)
+        self.attach_child(self.enemies)
+
+        self.position = self.property('TransformProperty').position
+
+        self.position.x = 256
+        self.position.y = 232
 
         # player = CharacterManager.create_character("character_edward")
         # goblin = CharacterManager.create_character("character_goblin")
@@ -35,7 +50,7 @@ class CharacterView(GameObject):
 
 
     def on_script(self):
-        if not self._initialized and GameLogic._initialized:
+        if not self._initialized and BattleLogic._initialized:
             self._initialize()
         else:
             pass
