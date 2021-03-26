2c2e3e8909e5bf353fe3129795659aab6db1fba4
Bachamut <lewand.luk@gmail.com>
2021-03-26 18:19:14 +0100

    turn queue rework v3.1

--- assets/lib/queue_model.py
+++ assets/lib/queue_model.py
@@ -3,7 +3,8 @@ class QueueModel():
     def __init__(self, character_model):
         self.cm = character_model
         self.temp_queue = []
-
+        self.end_cycle = {}
+        self.it = 0
 
     def create_queue(self):
         self.cm.units = self.cm.party_list + self.cm.enemy_list
@@ -12,20 +13,28 @@ class QueueModel():
             self.cm.character_list[unit] = speed
 
     def update_queue(self):
-        count = 9
+        count = 10
 
-        for n in range(0, count):
-            self.temp_queue.append(self._update_queue())
-        print(f'\n---QUEUE---')
-        for character in self.cm.queue_list:
-            print(character.name)
-        print(f'---END QUEUE---')
+        for n in range(0, count-len(self.temp_queue)):
+            self.temp_queue.append(self._get_next())
+        print(len(self.temp_queue))
+        # print(f'\n---QUEUE---')
+        # for character in self.cm.queue_list:
+        #     print(character.name)
+        # print(f'---END QUEUE---')
         next_char = self.temp_queue.pop(0)
-        self.cm.queue_list = self.temp_queue[0:8]
+        self.cm.queue_list = self.temp_queue[0:9]
         return next_char
 
-    def _update_queue(self):
+    def _get_next(self):
+
         while True:
+            if self.it == len(self.cm.character_list):
+                self.end_cycle = self.cm.character_list
+                print(f'---CYCLE END---')
+                for character, speed in self.cm.character_list.items():
+                    print(f'{character.name} speed = {speed}')
+                self.it = 0
             fastest_char = max(self.cm.character_list, key=self.cm.character_list.get)
             if self.cm.character_list[fastest_char] >= 100:
                 self.cm.character_list[fastest_char] -= 100
@@ -33,6 +42,7 @@ class QueueModel():
                 print("\nEND LOOP")
                 for character, speed in self.cm.character_list.items():
                     print(f'{character.name} speed = {speed}')
+                self.it += 1
                 return next_char
                 break
             else:
--- assets/lib/queue_view.py
+++ assets/lib/queue_view.py
@@ -27,11 +27,13 @@ class QueueView(GameObject):
         self.position.x = 400
         self.position.y = 20
 
-        BattleLogic = GameObject.get_object_pool().select_with_label('BattleLogic')[0]
+        battle_logic = GameObject.get_object_pool().select_with_label('BattleLogic')[0]
+        character_model = GameObject.get_object_pool().select_with_label('CharacterModel')[0]
+
         font = pygame.font.Font("assets/DisposableDroidBB.ttf", 16)
         self.line = TextLine.get_instance()
         self.line.set_font(font)
-        self.line.update(f'{BattleLogic.current_character.name}')
+        self.line.update(f'{battle_logic.current_character.name}')
         self.attach_child(self.line)
         self.line.property('SpriteProperty').visible = True
         position = self.line.property('TransformProperty').position
@@ -40,14 +42,38 @@ class QueueView(GameObject):
 
         self.line_current_char = TextLine.get_instance()
         self.line_current_char.set_font(font)
-        self.line_current_char.update(f'{BattleLogic.current_character.name}')
+        self.line_current_char.update(f'{battle_logic.current_character.name}')
         self.attach_child(self.line_current_char)
         self.line_current_char.property('SpriteProperty').visible = True
         position = self.line_current_char.property('TransformProperty').position
         position.y = 16
         position.x = 0
 
-
+    #     step = 0
+    #     for character in character_model.queue_list:
+    #         line = TextLine.get_instance()
+    #
+    #         font = pygame.font.Font("assets/DisposableDroidBB.ttf", 16)
+    #         line = TextLine.get_instance()
+    #         line.set_font(font)
+    #         line.update(f'{character.name}')
+    #         self.attach_child(line)
+    #         line.property('SpriteProperty').visible = True
+    #
+    #         position = line.property('TransformProperty').position
+    #         position.y = 0
+    #         position.y += step
+    #         step += 20
+    #         position.x = 0
+    #
+    # def create_line(self, character):
+    #     character_model = GameObject.get_object_pool().select_with_label('CharacterModel')[0]
+    #     font = pygame.font.Font("assets/DisposableDroidBB.ttf", 16)
+    #     self.line = TextLine.get_instance()
+    #     self.line.set_font(font)
+    #     self.line.update(f'{character.name}')
+    #     self.attach_child(self.line)
+    #     self.line.property('SpriteProperty').visible = True
 
     def on_create(self):
         pass
@@ -60,11 +86,11 @@ class QueueView(GameObject):
 
     def on_signal(self, signal):
         if signal.type == CHARACTER_CHANGED:
-            CharacterModel = GameObject.get_object_pool().select_with_label('CharacterModel')[0]
-            BattleLogic = GameObject.get_object_pool().select_with_label('BattleLogic')[0]
+            character_model = GameObject.get_object_pool().select_with_label('CharacterModel')[0]
+            battle_logic = GameObject.get_object_pool().select_with_label('BattleLogic')[0]
             line = ''
-            self.line_current_char.update(f'Current Character: {BattleLogic.current_character.name}')
-            for character in CharacterModel.queue_list:
+            self.line_current_char.update(f'Current Character: {battle_logic.current_character.name}')
+            for character in character_model.queue_list:
                 line += character.name + ', '
             self.line.update(f'Next: {line}')
 
