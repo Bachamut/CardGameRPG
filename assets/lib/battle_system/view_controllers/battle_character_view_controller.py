8b9940f4c9a3720e8b8b482542c88aff1a428f2a
Bachamut <lewand.luk@gmail.com>
2021-11-12 00:38:41 +0100

    Character animation .json

--- assets/lib/battle_system/view_controllers/battle_character_view_controller.py
+++ assets/lib/battle_system/view_controllers/battle_character_view_controller.py
@@ -37,7 +37,7 @@ class BattleCharacterViewController(GameObjectSharedResource):
             self.players.initialize(self.battle_ally[0])
             view_container.attach_child(self.players)
 
-            self.players.change_set('attack')
+            self.players.change_set('leap')
             self.players.scale(4)
 
             enemies = ObjectCreator.create_entity('battle_scene', 'Enemies')
@@ -57,21 +57,23 @@ class BattleCharacterViewController(GameObjectSharedResource):
 
     def on_script(self):
 
-        if self.animation_reset is False:
+        # if self.animation_reset is False:
+        #
+        #     if self.players.property('SpriteSheetAnimationProperty').frame >= self.players.max_frames:
+        #
+        #         self.animation_reset = True
+        #         self.players.change_set('leap')
+        #         self.players.scale(4)
+        #
+        # if self.animation_reset is True:
+        #
+        #     if self.players.property('SpriteSheetAnimationProperty').frame >= self.players.max_frames:
+        #
+        #         self.animation_reset = False
+        #         self.players.change_set('attack')
+        #         self.players.scale(4)
 
-            if self.players.property('SpriteSheetAnimationProperty').frame >= self.players.max_frames:
-
-                self.animation_reset = True
-                self.players.change_set('idle')
-                self.players.scale(4)
-
-        if self.animation_reset is True:
-
-            if self.players.property('SpriteSheetAnimationProperty').frame >= self.players.max_frames - 1:
-
-                self.animation_reset = False
-                self.players.change_set('attack')
-                self.players.scale(4)
+        pass
 
     def on_signal(self, signal):
 
